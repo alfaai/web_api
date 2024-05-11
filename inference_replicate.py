89a7df1717334253ed4a9a77f63ec5bc042f8d0d
alfa <alfaaistudio@gmail.com.com>
2024-05-11 08:18:54 +0300

    .

--- inference_replicate.py
+++ inference_replicate.py
@@ -1,4 +1,7 @@
 import replicate
+import os
+
+os.environ["REPLICATE_API_TOKEN"] = open("replicate_token").read()
 
 
 def sticker(prompt):
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from typing import Union
 from fastapi import FastAPI
 import inference_replicate as infrence
 
-
 app = FastAPI()
 
 
